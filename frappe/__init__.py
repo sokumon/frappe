3b4ef34a28ae060f4edbdcf4a48f18a229eb85bc
Akhil Narang <me@akhilnarang.dev>
2026-01-13 19:42:48 +0530

    fix: revert broken sed converting all `..` to `16.1.0`
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -853,7 +853,7 @@ def get_app_source_path(app_name, *joins):
 
 	:param app: App name.
 	:param *joins: Join additional path elements using `os.path.join`."""
-	return get_app_path(app_name, "16.1.0", *joins)
+	return get_app_path(app_name, "..", *joins)
 
 
 def get_site_path(*joins):
@@ -1152,7 +1152,7 @@ def get_newargs(fn: Callable, kwargs: dict[str, Any]) -> dict[str, Any]:
 
 	Example:
 	        >>> def fn(a=1, b=2):
-	        16.1.0.     pass
+	        ...     pass
 
 	        >>> get_newargs(fn, {"a": 2, "c": 1})
 	                {"a": 2}
